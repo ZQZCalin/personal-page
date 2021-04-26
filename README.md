eb40330693a071a173366500b4fa94090b33d086
ZQZCalin <zqz20100221@126.com>
2021-04-26 17:17:30 -0400

    JS user interaction

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # personal-page
+
+[Font Awesome](https://fontawesome.com/license) holds the copyright of all icons. No changes are made.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// global meta
+var newsMaxLength = 10;
+
 // fetch JSON
 fetch("data.json")
 .then(response => {
@@ -7,22 +10,38 @@ fetch("data.json")
     return response.json();
 })
 .then(data => {
-    // received fetch data, render page
-    renderMainPage(data);
+    // received fetch data
+    // // 1. Render Page
+    // renderMainPage(data);
+
+    // // 2. User Interaction
+    // userInteraction(data);
+
+    // check url parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("project")) {
+        // TO DO: render project detail (use a separate JSON)
+        let project = data.projects.find(item => item.id===urlParams.get("project"));
+        renderProjectPage(project);
+    } else {
+        renderMainPage(data);
+    }
+    userInteraction(data);
 })
 
 // =================
-// Render Functions
+// Render Main Page
 // =================
 
 function renderMainPage(data) {
     document.getElementById("home-container").innerHTML = `
         ${renderHeader()}
-        ${renderNavBar()}
+        ${renderNavBar(["About", "News", "Projects"], ["#section-about", "#section-news", "#section-projects"])}
         ${renderAbout(data.about)}
         ${renderNews(data.news)}
         ${renderProjects(data.projects)}
     `;
+    renderProjectItems(data.projects);
 }
 
 function renderHeader() {
@@ -35,13 +54,17 @@ function renderHeader() {
     `;
 }
 
-function renderNavBar() {
+function renderNavBar(menu, link) {
     return`
     <nav class="">
         <ul>
-            <li><a href="#section-about">About</a></li>
-            <li><a href="#section-news">News</a></li>
-            <li><a href="#section-projects">Projects</a></li>
+            ${
+                menu.map((section, i) => `
+                    <li><a href="${link[i]}">
+                        ${section}
+                    </a></li>
+                `).join("")
+            }
         </ul>
     </nav>
     `;
@@ -72,6 +95,7 @@ function renderAbout(about) {
             </p>
         </div>
     </section>
+    <hr class="solid">
     `;
 }
 
@@ -79,32 +103,58 @@ function renderNews(news) {
     return`
     <section id="section-news">
         <h1>News</h1>
-        <div class="row">
-            ${renderNewsItems(news)}
+        <div class="search">
+            <input type="search" name="news" placeholder="Search News...">
         </div>
+        <div id="news-list" class="row">
+            ${
+                news.map(newsItem => `
+                    <p class="col-8 news-title">${newsItem.title}</p>
+                    <p class="col-4 news-date">${newsItem.date}</p>
+                `).slice(0,newsMaxLength).join("")
+            }
+        </div>
+        <p></p>
     </section>
+    <hr class="solid">
     `;
 }
 
 function renderNewsItems(news) {
-    return news.map(newsItem => `
+    document.getElementById("news-list").innerHTML = news.map(newsItem => `
         <p class="col-8 news-title">${newsItem.title}</p>
         <p class="col-4 news-date">${newsItem.date}</p>
-    `).join("");
+    `).slice(0,newsMaxLength).join("");
 }
 
 function renderProjects(projects) {
     return`
     <section id="section-projects">
         <h1>Projects</h1>
+        <!-- Filter Bar -->
+        <div class="filter">
+            ${
+                ["all", "distributed systems", "machine learning"].map((tag, i) => `
+                <label>
+                    <input type="checkbox" name="projects" value="${tag}" ${(i==0)?"checked":""}>
+                    ${
+                        // String formatting: Uppercase Initials
+                        tag.split(" ")
+                        .map(word => word[0].toUpperCase()+word.slice(1).toLowerCase())
+                        .reduce((prev, word) => prev+" "+word)
+                    }
+                </label>
+                `).join("")
+            }
+        </div>
         <div id="project-container" class="row">
             <!-- Left Column -->
-            <div class="col-6">
-                ${renderProjectItems(projects, i => (i < projects.length/2))}
+            <div id="left-column" class="col-6">
+
             </div>
             <!-- Right Column -->
-            <div class="col-6">
-                ${renderProjectItems(projects, i => (i >= projects.length/2))}
+            <div id="right-column" class="col-6">
+
             </div>
         </div>
     </section>
@@ -112,9 +162,20 @@ function renderProjects(projects) {
 }
 
 // stack each item to either left of right
-function renderProjectItems(projects, projectSelector) {
-    return projects.map((project, i) => {
-        if (projectSelector(i)) {
+function renderProjectItems(projects) {
+    // threshold function
+    let threshold = i => i < projects.length / 2;
+    // left column
+    document.getElementById("left-column").innerHTML = 
+    projects.map((project, i) => {
+        if (threshold(i)) {
+            return renderProjectSingleItem(project);
+        }
+    }).join("");
+    // right column
+    document.getElementById("right-column").innerHTML = 
+    projects.map((project, i) => {
+        if (! threshold(i)) {
             return renderProjectSingleItem(project);
         }
     }).join("");
@@ -126,9 +187,9 @@ function renderProjectSingleItem(project) {
         <!-- Title -->
         ${
             // Mandatory: title
-            renderProjectTitle(project.title, project.link)
+            renderProjectTitle(project.title, project.id)
         }
-        <hr class="solid">
+        <hr class="solid project-dividor">
         <!-- Author -->
         ${
             // Mandatory: either String or Array
@@ -153,10 +214,10 @@ function renderProjectSingleItem(project) {
     `
 }
 
-function renderProjectTitle(title, link) {
-    if (link != undefined) {
+function renderProjectTitle(title, id) {
+    if (id != undefined) {
         return `
-        <a href="${link}" class="project-link">
+        <a href="?project=${id}" class="project-link">
             ${title}
         </a> <br>
         `;  
@@ -216,4 +277,77 @@ function renderProjectExternalLink(link) {
     let barPaper = (paper != undefined && (video != undefined || demo != undefined)) ? "|" : "";
     let barVideo = (video != undefined && demo != undefined) ? "|" : "";
     return `${paperIcon}${barPaper}${videoIcon}${barVideo}${demoIcon}`
+}
+
+// =================
+// Render Project
+// =================
+
+function renderProjectPage(project) {
+    document.getElementById("home-container").innerHTML = `
+        ${renderNavBar(["go back"], ["."])}
+        ${renderProjectSingleItem(project)}
+    `;
+}
+
+// =================
+// Render Project
+// =================
+
+function userInteraction(data) {
+    // search news
+    document.querySelector(".search input[name='news']")
+    .addEventListener("input", () => {UISearchNews(data.news)});
+    // filter projects
+    document.querySelectorAll(".filter input[name='projects']")
+    .forEach(box => {
+        box.addEventListener("change", () => {UIFilterProjects(box, data.projects)});
+    });
+}
+
+function UISearchNews(news) {
+    // filter news using title keyword
+    let keyword = document.querySelector(".search input[name='news']").value;
+    let filterNews = news.filter(item => item.title.toLowerCase().includes(keyword.toLowerCase()));
+    renderNewsItems(filterNews);
+}
+
+function UIFilterProjects(checkbox, projects){
+    // convert Nodelist to Array
+    let filter = Array.from(document.querySelectorAll(".filter input[name='projects']"));
+    let checkBoxAll = document.querySelector(".filter input[value='all']");
+    // Checkbox Logic
+    if (checkbox.value === "all" && checkbox.checked) {
+        // if "all" is selected, uncheck all other tags
+        filter.forEach((item, i) => {
+            if (i > 0) item.checked = false;
+        });
+    } else if (checkbox.checked) {
+        // if checked other tag, uncheck "all"
+        checkBoxAll.checked = false;
+    } else if (filter.every(tag => tag.checked==false)) {
+        // if no box is checked, check "all"
+        checkBoxAll.checked = true;
+    }
+    // Render
+    let selected = [];
+    filter.forEach(item => {
+        if (item.checked) selected.push(item.value);
+    });
+    if (selected.includes("all")) {
+        // render all
+        renderProjectItems(projects);
+    } else {
+        console.log(selected);
+        renderProjectItems(projects.filter(project => {
+            // check if a projected is tagged and contains any selected tag
+            if (project.tag != undefined && selected.length != 0) {
+                return selected.reduce((prev, tag) => {
+                    return prev || project.tag.includes(tag);
+                }, false);
+            } else {
+                return false;
+            }
+        }));
+    }
 }
