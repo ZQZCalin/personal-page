1f69188a48feaca835f615a6dde37f5c8be74cad
ZQZCalin <zqz20100221@126.com>
2021-04-27 16:47:23 -0400

    github page source

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,91 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
-const devMode = true;
+const devMode = false;
 
-module.exports = {
-    entry: {
-        index: "./src/index.js",
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        // auto clean "dist" folder
-        clean: true,
-    },
+if (devMode) {
+    module.exports = {
+        entry: {
+            index: "./src/index.js",
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            // auto clean "dist" folder
+            clean: true,
+        },
 
-    // Development Mode
-    mode: (devMode) ? "development" : "production",
-    devtool: (devMode) ? "inline-source-map":"",
-    devServer: {
-        contentBase: "./dist",
-    },
+        // Development Mode
+        mode: "development",
+        devtool: "inline-source-map",
+        devServer: {
+            contentBase: "./dist",
+        },
 
-    module: {
-        rules: [
-            {
-                // load css
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                // load image
-                test: /\.(png|svg|jpg|jpeg|gif)/i,
-                type: "asset/resource",
-            },
-            {
-                // load other assets (pdf)
-                test: /\.pdf/i,
-                type: "asset/resource",
-            },
+        module: {
+            rules: [
+                {
+                    // load css
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    // load image
+                    test: /\.(png|svg|jpg|jpeg|gif)/i,
+                    type: "asset/resource",
+                },
+                {
+                    // load other assets (pdf)
+                    test: /\.pdf/i,
+                    type: "asset/resource",
+                },
+            ],
+        },
+        plugins: [
+            // HTML plugin: auto generate "dist" folder
+            new HTMLWebpackPlugin({
+                // title: "Output Management",
+                title: "Development",
+            }),
         ],
-    },
-    plugins: [
-        // HTML plugin: auto generate "dist" folder
-        new HTMLWebpackPlugin({
-            // title: "Output Management",
-            title: "Development",
-        }),
-    ],
-};
+    };
+} else {
+    module.exports = {
+        entry: {
+            index: "./src/index.js",
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'docs'),
+            clean: true,
+        },
+
+        mode: "production",
+
+        module: {
+            rules: [
+                {
+                    // load css
+                    test: /\.css$/i,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    // load image
+                    test: /\.(png|svg|jpg|jpeg|gif)/i,
+                    type: "asset/resource",
+                },
+                {
+                    // load other assets (pdf)
+                    test: /\.pdf/i,
+                    type: "asset/resource",
+                },
+            ],
+        },
+        plugins: [
+            // HTML plugin: auto generate "dist" folder
+            new HTMLWebpackPlugin({
+                title: "Production",
+            }),
+        ],
+    };
+}
